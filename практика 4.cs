04019ffbb47825883d1fc9d26d4c58604ea8d4db
Iman200417 <115993776+Iman200417@users.noreply.github.com>
2022-10-17 16:54:56 +0500

    Add files via upload

---
+++ практика 4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace гаджиев_иман
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string neme
+            string twoname;
+            string animal;
+            string znak;
+            Console.WriteLine("Введите свое имя:");
+            neme = Console.ReadLine();
+            Console.WriteLine("Введите свою фамилию;");
+            twoname = Console.ReadLine();
+            Console.WriteLine("Введите свое любимое животное;");
+            animal = Console.ReadLine();
+            Consoie.Writline("Введите свой знак зодиака;");
+            znak = Console.ReadLine();
+            Console.WriteLine($"Индивидуальный гороскоп для пользователя) {neme} {twoname}");
+            Console.WriteLine($"Кем вы были в прошлой жизни: {animal}");
+            Console.WriteLine($"Ваш знак зодиака - {znak} , поэтому вы тонко чувствующая натура.");
+            
+            
+    }
+}
